38179b598607e2e0193c070f5048a3aa8a243fd0
Bhanu Rao <bhanu.rao@gmail.com>
2021-03-15 12:28:01 +0000

    Folder name change

--- admitme-lambda/src/util/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  clientId: "Admit-me", // for interacting with the NHS Login service
-  nhsRootDomain: "https://auth.sandpit.signin.nhs.uk",
-  privateKeyFilePath: "private_key.pem",
-};
--- admitme-react/src/config.js
+++
@@ -1,9 +0,0 @@
-const origin = window.location.origin
-
-export const  appConfig = {
-    clientId: 'Admit-me', 
-    nhsRootDomain: 'https://auth.sandpit.signin.nhs.uk',
-    scope: 'openid profile',
-    responseType: 'code',
-    redirectUri: origin + '/logincallback',
-  }
