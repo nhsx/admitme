e9c57bad4c8faea60584495b24af68a43aa20329
Bhanu Rao <bhanu.rao@gmail.com>
2021-03-15 14:31:44 +0000

    CORS issue on Lambda

--- admitme-userinfo/src/index.js
+++ admitme-userinfo/src/index.js
@@ -1,5 +1,5 @@
 const Handler = require("./api/Handler").Handler;
-const UserInfoService = require("./api/service/UserInfoService").UserInfoService;
+const UserInfoService = require("./api/service/userInfoService").UserInfoService;
 const handler = new Handler(new UserInfoService());
 
 exports.performUserInfoDetails = handler.handleUserInfoService;
