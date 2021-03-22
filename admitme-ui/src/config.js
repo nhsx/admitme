45d70ccb0897f9a754953e0747dfb5321d9970e7
Bhanu Rao <bhanu.rao@gmail.com>
2021-03-22 11:49:41 +0000

    Scope GP details removed

--- admitme-ui/src/config.js
+++ admitme-ui/src/config.js
@@ -3,7 +3,7 @@ const origin = window.location.origin
 export const  appConfig = {
     clientId: 'Admit-me', 
     nhsRootDomain: 'https://auth.sandpit.signin.nhs.uk',
-    scope: 'openid profile profile_extended email phone gp_registration_details',
+    scope: 'openid profile profile_extended email phone',
     responseType: 'code',
     redirectUri: origin + '/logincallback',
   }
