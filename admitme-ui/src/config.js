a20375c8d9015e926c621c111659e2e58f82d77f
Bhanu Rao <bhanu.rao@gmail.com>
2021-03-22 11:41:16 +0000

    Added additional scopes

--- admitme-ui/src/config.js
+++ admitme-ui/src/config.js
@@ -3,7 +3,7 @@ const origin = window.location.origin
 export const  appConfig = {
     clientId: 'Admit-me', 
     nhsRootDomain: 'https://auth.sandpit.signin.nhs.uk',
-    scope: 'openid profile',
+    scope: 'openid profile profile_extended email phone gp_registration_details',
     responseType: 'code',
     redirectUri: origin + '/logincallback',
   }
