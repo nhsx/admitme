c109049234ef4253977c23ddcbab400974bad71a
Trupti Joshi <Trupti14608@Mastek.Com>
2021-04-07 11:32:55 +0530

    cosmetic changes

--- admitme-pas/src/App.js
+++ admitme-pas/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Header transactional>
         <Header.Container>
           <Header.Logo href="/" />
-          <Header.ServiceName href="/">NHSX Mock PAS</Header.ServiceName>
+          <Header.ServiceName href="/">NHSX PAS</Header.ServiceName>
         </Header.Container>
       </Header>
       <Switch>
@@ -20,7 +20,7 @@ function App() {
       </Switch>
       <Footer>
         <Footer.List>
-          <Footer.ListItem href="/">NHSX Mock PAS</Footer.ListItem>
+          <Footer.ListItem href="/">NHSX PAS</Footer.ListItem>
         </Footer.List>
         <Footer.Copyright>&copy; Crown copyright</Footer.Copyright>
       </Footer>
