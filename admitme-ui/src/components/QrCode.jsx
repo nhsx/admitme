2321921bb92a4db27a8cba2ca3f13e55e8d7911f
Rob Walters <robert.walters1@hotmail.co.uk>
2021-04-20 16:17:38 +0100

    Fixed qr code sizing issues. Displays on all screens now.

--- admitme-ui/src/components/QrCode.jsx
+++ admitme-ui/src/components/QrCode.jsx
@@ -4,14 +4,19 @@ import qrCodeImage from "../img/QRCodeImage.svg";
 import { Link, useLocation } from "react-router-dom";
 import QRCode from "qrcode.react";//added by TRUPTI
 import props from 'prop-types';//added by TRUPTI
+import useWindowDimensions from "./useWindowDimensions";
 
 const goback = (paramcode) => {
   window.location.href = '/welcome?code=' + paramcode
 }
 
 export default function QrCode(props) {
+  const { windowHeight, windowWidth } = useWindowDimensions();
+  const smallestSize = windowWidth < windowHeight ? windowWidth : windowHeight // use the smallest window dimension to figure out how big the qr code should be to fit on screen
+  const qrCodeSize = smallestSize < 730 ? (smallestSize - 80) : 650; // max qr code size 650
   let code = '';
   let paramcode = '';
+
   if (typeof props.location.state != 'undefined') {
     code = props.location.state.code;
     paramcode = props.location.state.paramcode;
@@ -31,7 +36,7 @@ export default function QrCode(props) {
             <Row>
               <Col width="two-thirds">
                 <Label isPageHeading style={{ fontSize: '25px' }}>Share your details with NHS staff</Label>
-                <QRCode level='M' value={JSON.stringify(code)} id="canvas" size={350} />
+                <QRCode style={{'margin': '0 auto', 'display': 'block'}} level='M' value={JSON.stringify(code)} id="canvas" size={qrCodeSize}/>
               </Col>
             </Row>
           </Container>
