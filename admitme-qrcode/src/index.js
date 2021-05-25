0a62f677249814721620248ea9a4ca4366901d4d
Rob <rob.walters@nhsx.nhs.uk>
2021-05-25 15:14:59 +0100

    Removed admitme-qrcode - longer used

--- admitme-qrcode/src/index.js
+++
@@ -1,5 +0,0 @@
-const Handler = require("./api/Handler").Handler;
-const QrcodeService = require("./api/service/qrcodeService").QrcodeService;
-const handler = new Handler(new QrcodeService());
-
-exports.performQrCodeDetails = handler.handleQrcodeDetails;
--- admitme-qrcode/test/productDetailsService.spec.js
+++
@@ -1,9 +0,0 @@
-const LambdaTester = require("lambda-tester");
-const serviceQrcodeDetailsHandler = require("../src/index").performQrcodeDetails;
-
-describe("QR code", function () {
-  it("Is service running", () => {
-    // to be implemented
-    expect(true).toEqual(true);
-  });
-});
